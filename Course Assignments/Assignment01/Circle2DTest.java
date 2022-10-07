7951c7b0ece6ff049d1bb97393c750ce431adf27
Nishchal Jatwani <nishchal.jatwani@gmail.com>
2022-10-06 21:47:42 -0400

    added screenshots of output with final editing

--- Course Assignments/Assignment01/Circle2DTest.java
+++ Course Assignments/Assignment01/Circle2DTest.java
@@ -1,15 +1,24 @@
+// Declaration of main class
 public class Circle2DTest {
+
+    // Declaration of main method
     public static void main(String[] args) {
+        // Defining a circle c1 with the given arguments
         Circle2D c1 = new Circle2D(2,2,5.5);
 
+        // Printing the area using getter method for area
         System.out.println("The area of circle is: "+c1.getArea());
 
+        // Printing the perimeter using getter method for perimeter
         System.out.println("The perimeter of circle is: "+c1.getPerimeter());
 
+        // Checking if the points provided inside the circle and printing the final value in terminal
         System.out.println("Are the points in the circle: "+c1.contains(3, 3));
 
+        // Checking if the circle is enclosed in the circle and printing the value on terminal
         System.out.println("Is the circle enclosed in the provided circle: "+c1.contains(new Circle2D(4, 5, 10.5)));
 
+        // Checking if the circle overlaps each other and printing the value on terminal
         System.out.println("Does the circle overlaps the provide circle: "+c1.overlaps(new Circle2D(3, 5, 2.3)));
 
     }
--- Course Assignments/Assignment01/MyIntegerTest.java
+++ Course Assignments/Assignment01/MyIntegerTest.java
@@ -1,43 +1,53 @@
+// Declaration of class
 public class MyIntegerTest {
+
+    // Declaration of main method
     public static void main(String[] args) {
+        // Declaration of objects
         MyInteger integer1 = new MyInteger(1);
         MyInteger integer5 = new MyInteger(5);
         MyInteger integer$1 = new MyInteger(1);
 
+        // Printing value of integer using getter method
         System.out.println("Value of Integer 1 is: "+integer1.getValue());
         System.out.println("Value of Integer 5 is: "+integer5.getValue());
         System.out.println("Value of Integer $1 is: "+integer$1.getValue());
 
+        // Checking Even and Printing final value on the terminal 
         System.out.println("Integer 1 is even: "+integer1.isEven());
         System.out.println("Integer 5 is even: "+integer5.isEven());
 
+        // Checking Odd and Printing final value on the terminal
         System.out.println("Integer 1 is odd: "+integer1.isOdd());
         System.out.println("Integer 5 is odd: "+integer5.isOdd());
 
+        // Checking Prime and Printing final value on the terminal
         System.out.println("Integer 1 is prime: "+integer1.isPrime());
         System.out.println("Integer 5 is prime: "+integer5.isPrime());
 
+        // Checking even/odd/prime number using static method and printing final value on terminal
         System.out.println("The number 8 is even: "+MyInteger.isEven(8));
         System.out.println("The number 8 is odd: "+MyInteger.isOdd(8));
         System.out.println("The number 17 is prime: "+MyInteger.isPrime(17));
         System.out.println("The number 9 is prime: "+MyInteger.isPrime(9));
         
+        // Checking even/odd/prime object using static method and printing final value on terminal
         System.out.println("The integer 1 is even: "+MyInteger.isEven(integer1));
-
         System.out.println("The integer 5 is odd: "+MyInteger.isOdd(integer5));
-
         System.out.println("The integer 5 is prime: "+MyInteger.isPrime(integer5));
         System.out.println("The integer 1 is prime: "+MyInteger.isPrime(integer1));
 
+        // Comparing equality of two objects and printing final value on terminal
         System.out.println("The integer 1 is equal to 1: "+integer1.equals(1));
         System.out.println("The integer 5 is equal to 6: "+integer5.equals(6));
-
         System.out.println("Both integers 1 and 5 are equal: "+integer1.equals(integer5));
         System.out.println("Both integers 1 and $1 are equal: "+integer1.equals(integer$1));
 
+        // Parsing numeric array into int data type and printing it on terminal
         char[] charArray = {'1','2','3','4','5','6'};
         System.out.println(MyInteger.parseInt(charArray));
 
+        // Parsing string into int data type and printing it on terminal
         String string = "123456";
         System.out.println(MyInteger.parseInt(string));
     }
